a6b38bff0b0660562504aea6f11b32259a852377
federico1525 <f.casali@gmail.com>
2015-03-13 18:43:44 -0700

    Selenium Webdriver tests - exercises with Selection on Calendars

--- IDEtoWebdriver/src/tutorial/selenium/CalendarSelection.java
+++ IDEtoWebdriver/src/tutorial/selenium/CalendarSelection.java
@@ -17,11 +17,13 @@ public class CalendarSelection {
 
     private WebDriver driver;
     private String baseUrl;
+    private String baseUrl2;
 
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "http://www.expedia.com/";
+        baseUrl2 = "https://www.southwest.com/";
 
         // Maximize the browser's window
         driver.manage().window().maximize();
@@ -29,24 +31,47 @@ public class CalendarSelection {
     }
 
 
+//    @Test
+//    public void testDropdown() throws Exception {
+//        driver.get(baseUrl);
+//
+//        WebElement cal = driver.findElement(By.id("package-departing"));
+//        cal.click();
+//
+//        WebElement calDays = driver.findElement(By.xpath("//div[@class='cal']//section[1]//ul[@class='cal-dates']"));
+//
+//
+//        List<WebElement> days = calDays.findElements(By.tagName("li"));
+//
+//        for (WebElement giorno : days) {
+////            System.out.println(giorno.getText());
+//            if (giorno.getText().equals("30")) {
+////                Thread.sleep(1000);
+//                System.out.println("######");
+//                giorno.findElement(By.linkText("30")).click();
+//                break;
+//            }
+//        }
+//    }
+
     @Test
-    public void testDropdown() throws Exception {
-        driver.get(baseUrl);
+    public void swTestDropdown() throws Exception {
+        driver.get(baseUrl2);
 
-        WebElement cal = driver.findElement(By.id("package-departing"));
-        cal.click();
+        WebElement swcal = driver.findElement(By.id("air-date-departure"));
+        swcal.click();
 
-        WebElement calDays = driver.findElement(By.xpath("//div[@class='cal']//section[1]//ul[@class='cal-dates']"));
+        WebElement swCalDays = driver.findElement(By.xpath("//div[@class='calendar-1']//div[@class='calendar-month']//table[@class='calendar-table']"));
 
 
-        List<WebElement> days = calDays.findElements(By.tagName("li"));
+        List<WebElement> swdays = swCalDays.findElements(By.tagName("td"));
 
-        for (WebElement giorno : days) {
+        for (WebElement giorno : swdays) {
 //            System.out.println(giorno.getText());
             if (giorno.getText().equals("30")) {
 //                Thread.sleep(1000);
                 System.out.println("######");
-                giorno.findElement(By.linkText("30")).click();
+                giorno.click();  // not a link - apparently works just this way
                 break;
             }
         }
@@ -57,7 +82,7 @@ public class CalendarSelection {
 
     @After
     public void afterTest(){
-        driver.quit();
+//        driver.quit();
 
     }
 
