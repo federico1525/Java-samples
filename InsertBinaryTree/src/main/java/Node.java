9ef4df23a51b337ba164e849752dd55fe5761057
federico1525 <f.casali@gmail.com>
2015-04-07 22:50:31 -0700

    Separating Node class

---
+++ InsertBinaryTree/src/main/java/Node.java
@@ -0,0 +1,24 @@
+/**
+ * Created by fede on 4/7/15.
+ */
+public class Node {
+
+    int key;
+    String name;
+
+    Node leftChild;
+    Node rightChild;
+
+    Node(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    public String toString() {
+        return name + " has the key " + key;
+		/*
+         * return name + " has the key " + key + "\nLeft Child: " + leftChild +
+		 * "\nRight Child: " + rightChild + "\n";
+		 */
+    }
+}
